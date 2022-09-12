d746c98d4eb649eea4fa3ed79fcb2f4cc913daaa
Manjesh Gupta <gadgetsgearofficial@gmail.com>
2022-09-12 15:59:42 +0530

    db config updated

--- config/database.js
+++ config/database.js
@@ -1,19 +1,16 @@
 module.exports = ({ env }) => ({
-	defaultConnection: 'default',
-	connections: {
-		default: {
-			connector: 'bookshelf',
-			settings: {
-				client: 'postgres',
-				host: env('DATABASE_HOST', '127.0.0.1'),
-				port: env.int('DATABASE_PORT', 5432),
-				database: env('DATABASE_NAME', 'gadgetsgear'),
-				username: env('DATABASE_USERNAME', 'gadgetsgearroot'),
-				password: env('DATABASE_PASSWORD', 'bca7QXR5aut6ykw!nry'),
-			},
-			options: {
-				ssl: false,
-			},
-		},
-	},
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'gadgetsgear'),
+      user: env('DATABASE_USERNAME', 'gadgetsgearroot'),
+      password: env('DATABASE_PASSWORD', 'bca7QXR5aut6ykw!nry'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
+  },
 });
